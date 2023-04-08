3755e7bde543f37f8c5b579335066596ba7a3525
Shreyas0134 <shreyaskarandikar2000@gmail.com>
2023-04-08 16:26:55 +0530

    Updating phase

--- gamers-arena/src/components/EventPage.jsx
+++ gamers-arena/src/components/EventPage.jsx
@@ -10,7 +10,7 @@ const EventPage = () => {
           <p className='absolute bottom-28 left-0 right-0 text-center text-white text-base sm:text-xl md:text-6xl underline  leading-1 tracking-widest font-normal'>Organize your eSport event!</p>
         </div>
         <div className='w-full mx-auto text-center justify-center absolute top-0 mt-[8%] text-white'>
-          <p className='text-xl sm:text-2xl md:text-4xl lg:text-8xl font-extrabold italic'>Events</p>
+          <p className='text-xl sm:text-2xl md:text-4xl lg:text-8xl font-extrabold italic'>Events.</p>
           <h1 className='md:text-4xl sm:text-2xl text-xl font-bold md:py-16'>Tournaments & Exhibitions</h1>
         </div>
       </div>
