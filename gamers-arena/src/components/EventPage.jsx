e3dc1c04ec9e53b3cac21bf21c5c5a2eefb39be2
Shreyas0134 <shreyaskarandikar2000@gmail.com>
2023-04-20 21:05:56 +0530

    Updatin phase

--- gamers-arena/src/components/EventPage.jsx
+++ gamers-arena/src/components/EventPage.jsx
@@ -20,62 +20,79 @@ const EventPage = () => {
           <p className='flex justify-center font-bold text-3xl '>Old Events</p>
         </div>
         <div className=' grid sm:mx-auto  sm:grid-cols-2 lg:grid-cols-4 gap-2 text-center p-5'>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          {/* first card */}
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 transform hover:scale-105 ">
           <img src={img1} alt="" />
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Modena Nerd 2019</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto nisi quidem perspiciatis temporibus ipsam..</p>
           </div>
         </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+         {/* first card ends here */}
+          {/* second card */}
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 transform hover:scale-105 ">
           <img src={img1} alt="" />
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Modena Nerd 2019</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto nisi quidem perspiciatis temporibus ipsam..</p>
           </div>
         </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+         {/* second card ends here */}
+          {/* third card */}
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 transform hover:scale-105 ">
           <img src={img1} alt="" />
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Modena Nerd 2019</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto nisi quidem perspiciatis temporibus ipsam..</p>
           </div>
         </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+         {/* third card ends here */}
+          {/* forth card */}
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 transform hover:scale-105 ">
           <img src={img1} alt="" />
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Modena Nerd 2019</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto nisi quidem perspiciatis temporibus ipsam..</p>
           </div>
         </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+         {/* forth card ends here */}
+          {/* fifth card */}
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 transform hover:scale-105 ">
           <img src={img1} alt="" />
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Modena Nerd 2019</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto nisi quidem perspiciatis temporibus ipsam..</p>
           </div>
         </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+         {/* fifth card ends here */}
+          {/* sixth card */}
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 transform hover:scale-105 ">
           <img src={img1} alt="" />
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Modena Nerd 2019</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto nisi quidem perspiciatis temporibus ipsam..</p>
           </div>
         </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+         {/* sixth card ends here */}
+          {/* seventh card */}
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 transform hover:scale-105 ">
           <img src={img1} alt="" />
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Modena Nerd 2019</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto nisi quidem perspiciatis temporibus ipsam..</p>
           </div>
         </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+         {/* seventh card ends here */}
+          {/* Eighth card */}
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 transform hover:scale-105 ">
           <img src={img1} alt="" />
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Modena Nerd 2019</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto nisi quidem perspiciatis temporibus ipsam..</p>
           </div>
         </div>
+         {/* Eighth card ends here */}
+        
         </div>
       </div>
     </div>
